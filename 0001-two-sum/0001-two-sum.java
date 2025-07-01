a9db221ccb792159a6c8839b00443ead886a4b39
prince3585 <sahuprince114@gmail.com>
2025-07-01 21:35:44 +0530

    Time: 44 ms (36.92%), Space: 44.8 MB (88.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,8 +3,8 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target){
-                    int a[]={i,j};
-                    return a;
+                    
+                    return new int[]{i,j};
                 }
             }
         }
